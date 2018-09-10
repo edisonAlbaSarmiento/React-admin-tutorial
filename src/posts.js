19237255f24e9f491e0aa09a66c9c80b12207af0
Edison Alba <edison.alba@imaginamos.com.co>
2018-09-09 19:59:41 -0500

    [React Admin] Add Responsive y add API REST REAL

--- src/posts.js
+++ src/posts.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {Filter,List,Edit,Create, Datagrid, TextField,ReferenceField,EditButton,DisabledInput,LongTextInput,ReferenceInput,SelectInput,SimpleForm,TextInput} from 'react-admin';
+import {Filter,Responsive,SimpleList ,List,Edit,Create, Datagrid, TextField,ReferenceField,EditButton,DisabledInput,LongTextInput,ReferenceInput,SelectInput,SimpleForm,TextInput} from 'react-admin';
 
 export const PostList = (props) => (
   <List {...props} filters={<PostFilter />}>
-    <Datagrid>
-      <TextField source="id"/>
-      <ReferenceField label="User" source="userId" reference="users">
-      <TextField source="name"/>
-      </ReferenceField>
-      <TextField source="title"/>
-      <TextField source="body"/>
-      <EditButton />
-    </Datagrid>
+  <Responsive
+       small={
+           <SimpleList
+               primaryText={record => record.title}
+               secondaryText={record => `${record.views} views`}
+               tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+           />
+       }
+       medium={
+           <Datagrid>
+               <TextField source="id" />
+               <ReferenceField label="User" source="userId" reference="users">
+                   <TextField source="name" />
+               </ReferenceField>
+               <TextField source="title" />
+               <TextField source="body" />
+               <EditButton />
+           </Datagrid>
+       }
+   />
   </List>
 );
 
